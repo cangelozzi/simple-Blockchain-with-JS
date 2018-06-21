91fdac4d28cf48b70c64efff296419d4b3901a41
Camillo <cangelozzi@gmail.com>
2018-06-21 16:43:00 -0400

    adding Proof of Work / Mining

--- main.js
+++ main.js
@@ -14,6 +14,8 @@ class Block {
     this.previousHash = previousHash;
 
     this.hash = this.calculateHash();
+
+    this.nonce = 0; // nonse is a random number that CAN BE CHANGED in the Blockchain building process
   }
 
   calculateHash() {
@@ -21,15 +23,28 @@ class Block {
       this.index +
         this.previousHash +
         this.timestamp +
-        JSON.stringify(this.data)
+        JSON.stringify(this.data) +
+        this.nonce
     ).toString();
   }
+
+  //! PROOF OF WORK method to avoid quick creation of blocks, usually adding zeros
+  // more zeros more difficulty
+  mineBlock(difficulty) {
+    while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+      this.nonce++;
+      this.hash = this.calculateHash();
+    }
+    console.log("Block minded: " + this.hash);
+  }
+
 }
 
 // ! Create a BLOCKCHAIN to initialize the chain starting from GENESIS(first block) Block
 class Blockchain {
   constructor() {
     this.chain = [this.createGenesisBlock()];
+    this.difficulty = 4;
   }
 
   createGenesisBlock() {
@@ -44,8 +59,8 @@ class Blockchain {
     //! Get last hash from previous block
     newBlock.previousHash = this.getLatestBlock().hash;
 
-    //! Create the new hash for this block
-    newBlock.hash = newBlock.calculateHash();
+    //! Create the new hash for this block, adding proof of work/mining
+    newBlock.mineBlock(this.difficulty);
 
     //! Push Block to the chain
     this.chain.push(newBlock);
@@ -75,15 +90,17 @@ testCoin.addBlock(new Block(1, '19/06/2018', {amount: 7}));
 testCoin.addBlock(new Block(2, '06/08/2018', {amount: 4}));
 
 //! Test if the Blockchain is Valid ---> it should be TRUE
-console.log('is Blockchain valid? ' + testCoin.isChainvalid());
+//console.log('is Blockchain valid? ' + testCoin.isChainvalid());
 
 //! is Valid trying to change data (temper blockchain) ---> it should be FALSE
-testCoin.chain[1].data = {amount: 100}
-console.log("is Blockchain valid? " + testCoin.isChainvalid());
+// testCoin.chain[1].data = {amount: 100}
+// console.log("is Blockchain valid? " + testCoin.isChainvalid());
 
 //! is Valid trying to recalculate the hash ---> it should be FALSE
-testCoin.chain[1].hash = testCoin.chain[1].calculateHash();
-console.log("is Blockchain valid? " + testCoin.isChainvalid());
+// testCoin.chain[1].hash = testCoin.chain[1].calculateHash();
+// console.log("is Blockchain valid? " + testCoin.isChainvalid());
 
-//! Test the Blockchain and the linked hash
+//! Test the Blockchain and the linked hash, 
+//! Test if the Blockchain with Proof of Work / Mining, adding zeros at the beginning
 console.log(JSON.stringify(testCoin, null, 4)) // 4 spaces formatting
+
